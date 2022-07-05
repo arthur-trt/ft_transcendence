0b08a0c0080e6a8575c4615bccbf2dbed486d05e
Arhur-TRT <atrouill@student.42.fr>
2022-07-05 13:01:08 +0200

    Update to latest version

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/ash
 
-#cp -r /cache/node_modules.tar /api/
-# tar -xvf /cache/node_modules.tar -C /api/
-#ln -s /cache/node_modules /api/node_modules
-#cp -r /cache/node_modules/. /api/node_modules/
-
 npm install && npm audit fix
 
 exec npm run start:dev
