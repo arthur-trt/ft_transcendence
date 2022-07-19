f28697c430e8f99463e70b3ab1cc5b04efa6d3c6
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2022-07-19 15:43:00 +0200

    friendship tests

--- api/src/friendships/friendships.module.ts
+++ api/src/friendships/friendships.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FriendshipsController } from './friendships.controller';
 import { FriendshipsService } from './friendships.service';
+import { Friendships } from './frienship.entity';
 
 @Module({
   controllers: [FriendshipsController],
-  providers: [FriendshipsService]
+	providers: [FriendshipsService],
+	exports: [FriendshipsService],
+  imports : [TypeOrmModule.forFeature([Friendships])]
 })
 export class FriendshipsModule {}
