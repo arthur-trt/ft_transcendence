4713a131ab9cb4bf31a09364ac306a3628ca00d8
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2022-07-19 16:53:07 +0200

    adding event for friend reauest

--- api/src/dtos/sendPrivateMessageDto.dto.ts
+++ api/src/dtos/sendPrivateMessageDto.dto.ts
@@ -1,16 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
+import { User } from "src/user/user.entity";
 
 export class sendPrivateMessageDto
 {
 	@IsNotEmpty()
 	from: string;
-	
-	@IsNotEmpty()
-	username: string;
 
 	@IsNotEmpty()
-	socketId : string;
+	to: User;
 
 	@IsNotEmpty()
 	msg : string;
