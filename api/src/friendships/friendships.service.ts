044e5d8af5c9590f74296c9cf27b334f66333ac5
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2022-07-19 15:51:26 +0200

    friendship tests

--- api/src/friendships/friendships.service.ts
+++ api/src/friendships/friendships.service.ts
@@ -19,39 +19,38 @@ export class FriendshipsService {
 	}
 
 
-	public async acceptFriendRequest(user1: User, user2: User)
+	public async acceptFriendRequest(user1: User, user2: User) : Promise<Friendships>
 	{
 		const friendship = await this.friendRepo
 			.createQueryBuilder('friend')
 			.leftJoinAndMapOne("friend.sender", User, 'users', 'users.id = friend.sender')
 			.leftJoinAndMapOne("friend.target", User, 'usert', 'usert.id = friend.target')
 			.where(new Brackets(qb => {
-				qb.where("friend.sender :sender", { sender: user1.id })
+				qb.where("friend.sender = :sender", { sender: user1.id })
 					.orWhere("friend.sender = :sender2", { sender2: user2.id })
 			}))
 			.andWhere(new Brackets(qb => {
 				qb.where("friend.target = :dst", { dst: user1.id })
 					.orWhere("friend.target = :dst1", { dst1: user2.id })
 			}))
-			.select['status']
 			.getOne();
 
 		friendship.status = "accepted";
 		return await this.friendRepo.save(friendship);
 	}
 
-	public async getFriendsofUsers(user: User)
+	public async getFriendsofUsers(user: User) : Promise<Friendships[]>
 	{
 		return await this.friendRepo
-		.createQueryBuilder('friend')
-		.leftJoinAndMapOne("friend.sender", User, 'users', 'users.id = friend.sender')
-		.leftJoinAndMapOne("friend.target", User, 'usert', 'usert.id = friend.target')
-		.where("friend.sender : :sender", { sender : user.id })
-		.orWhere("friend.target : :target", { target : user.id })
-		.select(['friend.sender'])
+			.createQueryBuilder('friend')
+			.leftJoinAndMapOne("friend.sender", User, 'users', 'users.id = friend.sender')
+			.leftJoinAndMapOne("friend.target", User, 'usert', 'usert.id = friend.target')
+			.where("friend.status = :ok", { ok : "accepted"})
+			.where("friend.sender = :sender", { sender : user.id })
+			.orWhere("friend.target = :target", { target : user.id })
+			.select(['friend.sender'])
 			.addSelect([
 				'friend.target',
-				'friend.status',
 				'users.name',
 				'users.avatar_url',
 				'usert.name',
