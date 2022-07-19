5f796c4961bacab1a1f438675814983dd9366c1b
codebrai <debraixcorentin@gmail.com>
2022-07-19 16:10:11 +0200

    add game

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,48 +1,53 @@
-import React, { useState } from 'react';
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:8080/game');
-// import {socketo } from '../index'
+import React, { useState, useEffect } from 'react';
+import '../pong.css';
 
 export default function Game() {
 
   // const socket = socketo;
-  socket.on('init', () => { });
+  // socket.on('init', () => { });
 
-  const [name, setName] = useState("");
-  const [args, setArgs] = useState("");
-  let handleSubmit = (e: any) => {
-    e.preventDefault();
+  // const [name, setName] = useState("");
+  // const [args, setArgs] = useState("");
+  // let handleSubmit = (e: any) => {
+  //   e.preventDefault();
 
-    socket.emit(name, args);
+  //   socket.emit(name, args);
 
-    socket.onAny((event, ...args) => {
-      console.log(event, args);
-    });
+  //   socket.onAny((event, ...args) => {
+  //     console.log(event, args);
+  //   });
 
-    //window.location.reload();
-    setName("");
-    setArgs("");
+  //   //window.location.reload();
+  //   setName("");
+  //   setArgs("");
 
-  }
+  // }
 
   return (
-    <div className='dontknow'>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          placeholder="Message"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          value={args}
-          placeholder="Args"
-          onChange={(e) => setArgs(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
+
+    <div className='game-container'>
+      <div id="start">The game starts in 3 seconds ...</div>
+      <canvas id="pong"></canvas>
+      <script src="../game.ts"></script>
     </div>
+
+
+    // <div className='dontknow'>
+    //   <form onSubmit={handleSubmit}>
+    //     <input
+    //       type="text"
+    //       value={name}
+    //       placeholder="Message"
+    //       onChange={(e) => setName(e.target.value)}
+    //     />
+    //     <input
+    //       type="text"
+    //       value={args}
+    //       placeholder="Args"
+    //       onChange={(e) => setArgs(e.target.value)}
+    //     />
+    //     <button type="submit">Add</button>
+    //   </form>
+    // </div>
   )
 }
