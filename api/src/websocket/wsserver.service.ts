d64d83ca153715602f8e3bb8ae18bf8f4bc9b879
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2022-07-20 15:00:00 +0200

    protection if friend is not connected

--- api/src/websocket/wsserver.service.ts
+++ api/src/websocket/wsserver.service.ts
@@ -375,16 +375,22 @@ export class WSServer implements OnGatewayInit, OnGatewayConnection, OnGatewayDi
 	@SubscribeMessage('addFriend')
 	async addFriend(client: Socket, friend: User)
 	{
+		const friendSocket: string = (await this.findSocketId(friend)).id;
 		await this.friendService.sendFriendRequest(client.data.user, friend);
-		this.server.to((await this.findSocketId(friend)).id).emit('newFriendRequest', "You have a new friend request", client.data.user)
+		if (friendSocket)
+			this.server.to(friendSocket).emit('newFriendRequest', "You have a new friend request", client.data.user)
 	}
 
 	@UseGuards(WsJwtAuthGuard)
 	@SubscribeMessage('acceptFriend')
 	async acceptFriendRequest(client: Socket, friend: User)
 	{
+		const friendSocket: string = (await this.findSocketId(friend)).id;
 		await this.friendService.acceptFriendRequest(client.data.user, friend);
-		this.server.to((await this.findSocketId(friend)).id).to(client.id).emit('friendList', "Friend list", await this.friendService.getFriendsofUsers(client.data.user));
+		if (friendSocket)
+			this.server.to(friendSocket).to(client.id).emit('friendList', "Friend list", await this.friendService.getFriendsofUsers(client.data.user));
+		else
+			this.server.to(client.id).emit('friendList', "Friend list", await this.friendService.getFriendsofUsers(client.data.user));
 	}
 
 
@@ -392,8 +398,12 @@ export class WSServer implements OnGatewayInit, OnGatewayConnection, OnGatewayDi
 	@SubscribeMessage('removeFriend')
 	async removeFriend(client: Socket, friend: User)
 	{
+		const friendSocket: string = (await this.findSocketId(friend)).id;
 		await this.friendService.removeFriend(client.data.user, friend);
-		this.server.to(client.id).emit('friendList', "Friend list", await this.friendService.getFriendsofUsers(client.data.user));
+		if (friendSocket)
+			this.server.to(friendSocket).to(client.id).emit('friendList', "Friend list", await this.friendService.getFriendsofUsers(client.data.user));
+		else
+			this.server.to(friendSocket).emit('friendList', "Friend list", await this.friendService.getFriendsofUsers(client.data.user));
 	}
 
 	@UseGuards(WsJwtAuthGuard)
