0bc610cb46527ee866abd7dc839ddfcd0adc26b2
Arhur-TRT <atrouill@student.42.fr>
2022-07-21 10:15:20 +0200

    Stupid useless thing

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -49,11 +49,21 @@ export class CheatAuthController {
 		const { data } = await firstValueFrom(this.httpService.get("https://api.namefake.com/"));
 		const fake = JSON.parse(JSON.stringify(data));
 
+		const image: string[] = [
+			"https://fr.web.img6.acsta.net/r_1920_1080/medias/nmedia/18/62/48/25/18645943.jpg",
+			"https://img.phonandroid.com/2018/11/xavier-niel-portrait.jpg",
+			"https://upload.wikimedia.org/wikipedia/commons/e/eb/Joseph_Stalin_at_the_Tehran_conference_on_1943.jpg",
+			"https://upload.wikimedia.org/wikipedia/commons/a/a6/Nicolas_Sarkozy_in_2010.jpg",
+			"https://upload.wikimedia.org/wikipedia/commons/d/de/Bernard_Arnault_%283%29_-_2017_%28cropped%29.jpg",
+			"https://www.challenges.fr/assets/img/2016/06/07/cover-r4x3w1000-59c3e4252e6e8-liliane-bettencourt.jpg",
+
+		]
+
 		const user = await this.userService.findOrCreateUser(
 			Math.floor(100000 + Math.random() * 900000),
 			fake.name,
 			fake.username,
-			"https://fr.web.img6.acsta.net/r_1920_1080/medias/nmedia/18/62/48/25/18645943.jpg",
+			image[Math.floor(Math.random()*image.length)],
 			fake.email_u + "@" + fake.email_d
 		)
 
