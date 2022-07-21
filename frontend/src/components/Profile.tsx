5f9b659f874594b8a4e7eab6966959a6a2666571
Arhur-TRT <atrouill@student.42.fr>
2022-07-21 16:26:10 +0200

    Upload avatar

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -9,33 +9,32 @@ export default function Profile() {
     const [data, setData] = useState<any>([]);
     const [name, setName] = useState("");
     const [inputState, setInputState] = useState(0);
-    const [file, setFile] = useState<any>();
 
     useEffect(() => {
-    const getData = async () => {
-        const response = await fetch(
-            `/api/user/me`
-        );
-        let actualData = await response.json();
-        setData(actualData);
-    }
-    getData()
+        const getData = async () => {
+            const response = await fetch(
+                `/api/user/me`
+            );
+            let actualData = await response.json();
+            setData(actualData);
+        }
+        getData()
     }, [])
 
-    let handleChangeName = (e:any) => {
+    let handleChangeName = (e: any) => {
         e.preventDefault();
 
         fetch('/api/user/userSettings', {
-        method: 'PATCH',
-        body: JSON.stringify({
-            name: name,
-            mail: data.mail,
-            fullname: data.fullname,
-            avatar_url: data.avatar_url
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
+            method: 'PATCH',
+            body: JSON.stringify({
+                name: name,
+                mail: data.mail,
+                fullname: data.fullname,
+                avatar_url: data.avatar_url
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
         })
 
         setName("");
@@ -48,56 +47,47 @@ export default function Profile() {
     }
 
     function displayInputName() {
-    if (inputState % 2 === 1)
-    {
-        return (
-            <form onSubmit={handleChangeName}>
-                <input
-                type="text"
-                value={name}
-                placeholder="Pseudo..."
-                onChange={(e) => setName(e.target.value)}
-                />
-            </form>
-        );
-    }
-    if (inputState % 2 === 0)
-        return("");
-    }
-
-    function handleChangeAvatar(event:any) {
-        setFile(event.target.files[0])
+        if (inputState % 2 === 1) {
+            return (
+                <form onSubmit={handleChangeName}>
+                    <input
+                        type="text"
+                        value={name}
+                        placeholder="Pseudo..."
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </form>
+            );
+        }
+        if (inputState % 2 === 0)
+            return ("");
     }
 
-    function handleSubmitAvatar(event:any) {
+    function handleSubmitAvatar(event: any) {
         event.preventDefault();
-        // console.log(file);
-        
-        // // var postFile = new FormData();
-        // postFile.append('file', file);
+
+        var postFile = new FormData();
+        postFile.append('file', event.target[0].files[0]);
 
         fetch('/api/user/avatar', {
             method: 'POST',
-            body: file,
-            headers: {
-                'Content-type': 'multipart/form-data',
-            },
-            })
-      }
+            body: postFile,
+        })
+    }
 
     return (
         <div className="profile-container">
             <div className="profile-img">
                 <img src={data.avatar_url}></img>
                 <form onSubmit={handleSubmitAvatar}>
-                    <input type="file" onChange={handleChangeAvatar}/>
+                    <input type="file" />
                     <button type="submit">Upload</button>
                 </form>
             </div>
             <div className="profile-info">
                 <div className="profile-name">
                     <h5>PSEUDO : {data.name}</h5>
-                    <FontAwesomeIcon icon={faPen} className="pen" onClick={handleInputName}/>
+                    <FontAwesomeIcon icon={faPen} className="pen" onClick={handleInputName} />
                     {displayInputName()}
                 </div>
                 <div className="profile-other">
