7494a260635453923893992f7e7131a3cbcabe22
Arhur-TRT <atrouill@student.42.fr>
2022-07-21 18:30:43 +0200

    Ad 2fA

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -4,11 +4,19 @@ import '../index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPen } from '@fortawesome/free-solid-svg-icons'
 
+
+
 export default function Profile() {
 
     const [data, setData] = useState<any>([]);
     const [name, setName] = useState("");
     const [inputState, setInputState] = useState(0);
+    const [errorMsg, setErrorMsg] = useState<string>("");
+    const [twofaCheck, setTwofaCheck] = useState<boolean>(false);
+    const [twoFaSecret, setTwoFaSecret] = useState<string>();
+    const [twoFaImage, setTwoFaImage] = useState<string>();
+    const [code, setCode] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
 
     useEffect(() => {
         const getData = async () => {
@@ -17,6 +25,8 @@ export default function Profile() {
             );
             let actualData = await response.json();
             setData(actualData);
+            if (actualData.TwoFA_enable == true)
+                setTwofaCheck(true);
         }
         getData()
     }, [])
@@ -30,7 +40,8 @@ export default function Profile() {
                 name: name,
                 mail: data.mail,
                 fullname: data.fullname,
-                avatar_url: data.avatar_url
+                avatar_url: data.avatar_url,
+                two_fa: data.TwoFA_enable
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -63,17 +74,59 @@ export default function Profile() {
             return ("");
     }
 
-    function handleSubmitAvatar(event: any) {
+    async function handleSubmitAvatar(event: any) {
         event.preventDefault();
 
         var postFile = new FormData();
         postFile.append('file', event.target[0].files[0]);
 
-        fetch('/api/user/avatar', {
+        const response = await fetch('/api/user/avatar', {
             method: 'POST',
             body: postFile,
         })
+        if (!response.ok) {
+            if (response.status == 415)
+                setErrorMsg("JS c'est du caca");
+        }
+        else {
+            window.location.reload();
+        }
     }
+
+    async function handleTwoFAChange(event: any) {
+        const secret = await (await fetch(
+            "/api/auth/2fa/generate"
+        )).json();
+        setTwoFaSecret(secret.secret);
+        setTwoFaImage(secret.qrcode);
+    }
+
+    async function handleTurnOnTwoFa(e: any) {
+        e.preventDefault();
+        try {
+            let res = await fetch("/api/auth/2fa/turn-on", {
+              method: "POST",
+              body: JSON.stringify({
+                token: code,
+              }),
+              headers: {
+                  "Content-Type": "application/json",
+              },
+            });
+             let resJson = await res.json();
+            if (res.status !== 201) {
+              setMessage(resJson.message);
+            }
+            else{
+                setTwoFaSecret("");
+                setTwofaCheck(true);
+            }
+          } catch (err) {
+            console.log(err);
+          }
+    }
+
+
 
     return (
         <div className="profile-container">
@@ -83,6 +136,7 @@ export default function Profile() {
                     <input type="file" />
                     <button type="submit">Upload</button>
                 </form>
+                {errorMsg && <h3 className="error"> {errorMsg} </h3>}
             </div>
             <div className="profile-info">
                 <div className="profile-name">
@@ -90,13 +144,40 @@ export default function Profile() {
                     <FontAwesomeIcon icon={faPen} className="pen" onClick={handleInputName} />
                     {displayInputName()}
                 </div>
+                <div className="twofa_user">
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={twofaCheck}
+                            onChange={handleTwoFAChange}
+                        />
+                        TwoFA Enable
+                    </label>
+                    {twoFaSecret &&
+                        <div className="secret">
+                            <img src={twoFaImage} alt="TwoFA QRCode Image" />
+                            <p>{twoFaSecret}</p>
+                            <form onSubmit={handleTurnOnTwoFa}>
+                                <input
+                                    type="text"
+                                    value={code}
+                                    placeholder="Validate your code"
+                                    onChange={(e) => setCode(e.target.value)}
+                                />
+                                <button type="submit">Validate</button>
+                                <div className="message">{message ? <p>{message}</p> : null}</div>
+                            </form>
+                        </div>
+                    }
+
+                </div>
                 <div className="profile-other">
                     <h5>FULL NAME : {data.fullname}</h5>
                     <h5>MAIL : {data.mail}</h5>
                     <h5>VICTORY : {data.wonMatches}</h5>
                 </div>
             </div>
-        </div>
+        </div >
     )
 
 }
