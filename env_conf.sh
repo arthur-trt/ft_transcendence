0d7d90a542ad09385a36a791456d237136d6f37c
Arthur T <atrouill@student.42.fr>
2022-07-28 12:43:56 +0200

    223 make everything compatible with shitty 42 si (#225)
    * Itś working
    
    * Oups

--- env_conf.sh
+++ env_conf.sh
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+#!/bin/bash
+
+=======
+>>>>>>> origin/main
 function	check_env_file() {
 	if [ -f ".env" ]; then
 		read -p "Env file already exist, do you want to erase it ? " -n 1 -r
@@ -90,7 +95,7 @@ function	build_other() {
 
 read_42api_secret
 
-if [[ check_fortytwo ]]; then
+if check_fortytwo; then
 	build_fortytwo
 else
 	build_other
