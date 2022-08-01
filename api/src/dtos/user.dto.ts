fbd8e0ee7a8ac3b149eb660cb782bdf01156eab8
ccommiss <41895626+Ccommiss@users.noreply.github.com>
2022-08-01 16:51:08 +0200

    fix ok but cannot handle the exception (#291)

--- api/src/dtos/user.dto.ts
+++ api/src/dtos/user.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsEmpty, isNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, IsEmpty, isNotEmpty, Length } from "class-validator";
 import {v4 as uuidv4} from 'uuid';
 
 export class ModifyUserDto {
 
 	@IsNotEmpty()
+	@Length(3, 20)
 	name: string;
 
 	@IsNotEmpty()
@@ -12,6 +13,7 @@ export class ModifyUserDto {
 	mail: string;
 
 	@IsNotEmpty()
+	@Length(3, 20)
 	fullname: string;
 
 	@IsNotEmpty()
