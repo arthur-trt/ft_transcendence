8e2d46b18690d9730a6e2a545436a5b0cf985c1e
hdurand-98 <71292323+hdurand-98@users.noreply.github.com>
2022-08-02 13:26:13 +0200

    Update api/src/websocket/game.relayService.ts
    Co-authored-by: Arthur T <atrouill@student.42.fr>

--- api/src/websocket/game.relayService.ts
+++ api/src/websocket/game.relayService.ts
@@ -56,7 +56,7 @@ export class GameRelayService
         protected player2 = {} as Paddle;
         protected p1_score = 0;
         protected p2_score = 0;
-        protected loop_stop : any;
+        protected loop_stop : number;
         protected players_ready = 0;
 
         protected P1_MoveUP : boolean;
