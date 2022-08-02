9a0d3ff658adad941ef8d9dbdb4f09bbbb425afa
Arthur T <atrouill@student.42.fr>
2022-08-02 18:09:41 +0200

    Revert "Check logged user (#312)" (#316)
    This reverts commit 2562a02373ca6efd058cc80f14f9f3c5792f3824.

--- api/src/websocket/exception.filter.ts
+++ api/src/websocket/exception.filter.ts
@@ -6,26 +6,26 @@ import { QueryFailedError } from "typeorm";
 
 @Catch(WsException, HttpException, QueryFailedError)
 export class WebsocketExceptionsFilter extends BaseWsExceptionFilter {
-	catch(exception: WsException | HttpException | QueryFailedError, host: ArgumentsHost) {
+  catch(exception: WsException | HttpException | QueryFailedError , host: ArgumentsHost) {
 
-		const client = host.switchToWs().getClient() as Socket;
-		const data = host.switchToWs().getData();
-		let error;
-		if (exception instanceof WsException)
-			error = exception.getError()
-		else if (exception instanceof BadRequestException) // pour les DTO
-		{
-			error = exception.getResponse();
-			if (typeof (error) == 'object')
-				error = error.message[0]; // a chaque fois on prend juste le premier pour assurere un meilleur formatage
-		}
-		else if (exception instanceof HttpException)
-			error = exception.getResponse();
-		else if (exception instanceof QueryFailedError)
-			error = "Query failed [" + exception.parameters + "] : " + exception.message;
-		const details = error instanceof Object ? { ...error } : { message: error };
-		client.emit("error", { event: error, data: data })
+  const client = host.switchToWs().getClient() as Socket;
+	  const data = host.switchToWs().getData();
+	let error;
+	if (exception instanceof WsException)
+		error = exception.getError()
+	else if (exception instanceof BadRequestException) // pour les DTO
+	{
+		  error = exception.getResponse();
+		  if (typeof (error) == 'object')
+			  error = error.message[0]; // a chaque fois on prend juste le premier pour assurere un meilleur formatage
 	}
+	  else if (exception instanceof HttpException)
+		  error = exception.getResponse();
+	  else if (exception instanceof QueryFailedError)
+		  error = "Query failed [" + exception.parameters + "] : " + exception.message;
+	const details = error instanceof Object ? { ...error } : { message: error };
+	client.emit("error", { event : error, data : data })
+  }
 }
 
 
