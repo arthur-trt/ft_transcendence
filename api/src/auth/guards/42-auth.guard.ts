c5f00d71d5250e1a10f789800e4f8b9aa3dd34ce
Arhur-TRT <atrouill@student.42.fr>
2022-08-02 14:08:01 +0200

    Handle error when user refuse OAuth

--- api/src/auth/guards/42-auth.guard.ts
+++ api/src/auth/guards/42-auth.guard.ts
@@ -1,19 +1,15 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { Response } from "express";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class FortyTwoAuthGuard extends AuthGuard('42') {
-	//handleRequest<TUser = any>(err: any, user: any, info: any, context: any, status?: any): TUser {
-	//	if (err || !user)
-	//	{
-	//		throw err || new HttpException('Caca boudin', HttpStatus.UNAUTHORIZED);;
-	//	}
-	//	return user;
-	//}
-	//canActivate(context: ExecutionContext): boolean {
-	//	const request = context.switchToHttp().getRequest();
-	//	console.log(request);
-	//	return true;
-	//}
+	handleRequest<TUser = any>(err: any, user: any, info: any, context: any, status?: any): TUser {
+		if (err || !user)
+		{
+			return null;
+		}
+		return user;
+	}
 }
