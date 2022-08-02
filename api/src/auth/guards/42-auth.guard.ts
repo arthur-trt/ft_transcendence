bb94fdcffc4d81f5de3d6ad7db109c25bab11f74
Arhur-TRT <atrouill@student.42.fr>
2022-08-02 14:13:51 +0200

    Remove type

--- api/src/auth/guards/42-auth.guard.ts
+++ api/src/auth/guards/42-auth.guard.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
 export class FortyTwoAuthGuard extends AuthGuard('42') {
-	handleRequest<TUser = any>(err: any, user: any, info: any, context: any, status?: any): TUser {
+	handleRequest<TUser = any>(err, user): TUser {
 		if (err || !user)
 		{
 			return null;
