66192784634a7099b094f4eca11e8560bb60953d
Arhur-TRT <atrouill@student.42.fr>
2022-08-03 17:04:06 +0200

    Change from href to useLocation()

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import 'font-awesome/css/font-awesome.min.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useCookies } from "react-cookie";
 
 export const Header = () => {
 
     const [data, setData] = useState<any>([]);
-    let current_url = window.location.href.substring(21);
-    const [cookies, setCookie, removeCookie] = useCookies();
+    const location = useLocation().pathname;
+    const [cookies, , removeCookie] = useCookies();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -36,13 +36,13 @@ export const Header = () => {
                     <Link to="/" style={{ textDecoration: 'none' }}><img src="bplogo.png" alt="bp logo"></img></Link>
                 </div>
                 <div className="onglets">
-                    <h3 style={{borderBottom: current_url==="/community" ? '3px solid #1dd1a1' : '', }}><Link to="/community" style={{ textDecoration: 'none', color: 'black' }}>COMMUNITY</Link></h3>
-                    <h3 style={{borderBottom: current_url==="/game" ? '3px solid #1dd1a1' : '', }}><Link to="/game" style={{ textDecoration: 'none', color: 'black' }}>GAME</Link></h3>
-                    <h3 style={{borderBottom: current_url==="/ladder" ? '3px solid #1dd1a1' : '', }}><Link to="/ladder" style={{ textDecoration: 'none', color: 'black' }}>LADDER</Link></h3>
+                    <h3 style={{borderBottom: location==="/community" ? '3px solid #1dd1a1' : '', }}><Link to="/community" style={{ textDecoration: 'none', color: 'black' }}>COMMUNITY</Link></h3>
+                    <h3 style={{borderBottom: location==="/game" ? '3px solid #1dd1a1' : '', }}><Link to="/game" style={{ textDecoration: 'none', color: 'black' }}>GAME</Link></h3>
+                    <h3 style={{borderBottom: location==="/ladder" ? '3px solid #1dd1a1' : '', }}><Link to="/ladder" style={{ textDecoration: 'none', color: 'black' }}>LADDER</Link></h3>
                 </div>
                 <div className="info">
                     <img src={data.avatar_url} alt="user"></img>
-                    <h1 style={{borderBottom: current_url==="/profile/me" ? '3px solid #1dd1a1' : '', }}>{data.name}</h1>
+                    <h1 style={{borderBottom: location==="/profile/me" ? '3px solid #1dd1a1' : '', }}>{data.name}</h1>
                     <Link to="/profile/me" style={{ textDecoration: 'none', color: 'black' }}>
                         <i className="fa fa-solid fa-user"></i>
                     </Link>
