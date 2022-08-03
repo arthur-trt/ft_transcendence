f97b29a93df8a814f1f6f7f2d7db4d74a826c9a1
Arhur-TRT <atrouill@student.42.fr>
2022-08-03 10:54:17 +0200

    Remove import useless

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
-import { Channel } from './channel/channel.entity';
 import { ChannelModule } from './channel/channel.module';
-import { privateMessage } from './message/privateMessage.entity';
-import { channelMessage } from './message/channelMessage.entity';
 import { AuthModule } from './auth/auth.module';
-// import { ChatModule } from './websocket/chat.module';
-import { MatchHistory } from './game/game.entity';
 import { GameModule } from './game/game.module';
-import { UserActivity } from './user/user_activity.entity';
 import { FriendshipsModule } from './friendships/friendships.module';
 import { WSServerModule } from './websocket/wsserver.module'
 import { AchievementsModule } from './achievements/achievements.module';
@@ -21,7 +12,7 @@ import { AchievementsModule } from './achievements/achievements.module';
 @Module({
 
 	imports: [
-		ConfigModule.forRoot({ //<- imported the config module
+		ConfigModule.forRoot({
 			envFilePath: '.env',
 		}),
 		UserModule,
@@ -37,8 +28,6 @@ import { AchievementsModule } from './achievements/achievements.module';
 			password: process.env.POSTGRES_PASSWORD,
 			database: process.env.POSTGRES_DB,
 			autoLoadEntities: true,
-			// entities: //[User, Channel, privateMessage, channelMessage, MatchHistory, UserActivity], /* Create tables according to prototype in entities */
-			//entities: [ './car/*.entity.{js,ts}'],
 			synchronize: true,
 			logging: false
 		}),
