8a60c54d2673ed5b8c9d1a2757a8614660ed7e10
ccommiss <ccommiss@student.42.fr>
2022-08-12 13:00:11 +0200

    controllers

--- api/src/achievements/achievements.controller.ts
+++ api/src/achievements/achievements.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { achievementDto } from 'src/dtos/achievement.dto';
+import { uuidDto } from 'src/dtos/uuid.dto';
 import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
 import { AchievementsService } from './achievements.service';
@@ -16,6 +17,14 @@ export class AchievementsController {
 	public async getAchievements(@Req() req : Request)
 	{
 		const user: User = await this.userService.getUserByRequest(req);
+		return await this.SuccessService.getAchievements(user);
+	}
+
+	@Get(':uuid')
+	@UseGuards(JwtAuthGuard)
+	public async getAchievementsofUser(@Param() uuid: uuidDto)
+	{
+		const user: User = await this.userService.getUserByIdentifier(uuid.uuid);
 		return await this.SuccessService.getAchievements(user);
 	}
 
